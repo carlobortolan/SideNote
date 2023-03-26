ae9dd33a518de97c2f886eacd7b9f3f093f16c50
carlobortolan <carlobortolan@gmail.com>
2023-03-26 21:25:17 +0200

    Updated routes and route access

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl text-green-500">SideNote</h1>
+      <h1 className="text-2xl text-green-500">SideNote</h1>
+      <button className="h10 w-full bg-white" onClick={() => signOut()}>
+        Sign Out
+      </button>
     </>
-  )
+  );
 }
