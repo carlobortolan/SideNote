0ccc6f1a8da0f0446f780621ad262f37a6578d8c
carlobortolan <carlobortolan@gmail.com>
2023-03-26 21:48:05 +0200

    Added profiles page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -17,9 +18,12 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-2xl text-green-500">SideNote</h1>
+      <p className="text-white">Logged in as: {user?.email}</p>
       <button className="h10 w-full bg-white" onClick={() => signOut()}>
         Sign Out
       </button>
