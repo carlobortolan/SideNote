5d416c1f14c6b16816e4f931dbbce41cce065e37
carlobortolan <carlobortolan@gmail.com>
2023-03-27 01:09:59 +0200

    Added preview fields

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { getSession } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   if (!(await getSession(context))) {
@@ -17,12 +19,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
